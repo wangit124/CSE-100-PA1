4805a7e0e4e94965feb2244c6ab33f77a2f5130a
Luhao Wang <luw055@ieng6-202.ucsd.edu>
2019-04-10 10:32:00 -0700

    Added initial styling and constructor method

--- BST/BSA.hpp
+++ BST/BSA.hpp
@@ -1,3 +1,12 @@
+/* Description: This is a program that defines and implements
+ * methods used for binary search arrays, including bsearch,
+ * find, insert, and iterator methods begin and end.
+ *
+ * Name: Luhao Wang
+ * Account: luw055
+ * Date: April 10, 2019
+ */
+
 #ifndef BSA_HPP
 #define BSA_HPP
 
@@ -8,38 +17,39 @@ template<typename Data>
 
 /** This class implements a binary search array */
 class BSA {
-    
-    private:
-        vector<Data> v;
 
-        /** Helper method used to either find an element if it exists
-         * or else find where it should be inserted.
-         */
-        virtual size_t binarySearch(const Data& item) const {
-            //TODO
-            return 0;
-        }
+	private:
+		vector<Data> v;
 
-    public:
-        /** Return the position of item, otherwise v.cend() */
-        virtual typename vector<Data>::const_iterator find(const Data& item) const {
-            //TODO
-            return v.cend();
-        }
+		/** Helper method used to either find an element if it exists
+		 * or else find where it should be inserted.
+		 */
+		virtual size_t binarySearch(const Data& item) const {
+			//TODO
+			return 0;
+		}
 
-        /** Insert item into sorted position */
-        virtual bool insert(const Data& item) {
-            //TODO
-            return false;
-        }
+	public:
+		/** Return the position of item, otherwise v.cend() */
+		virtual typename vector<Data>::const_iterator find(const Data& item) 
+													const {
+			//TODO
+			return v.cend();
+		}
 
-        typename vector<Data>::iterator begin() const {
-            return v.begin();
-        }
+		/** Insert item into sorted position */
+		virtual bool insert(const Data& item) {
+			//TODO
+			return false;
+		}
 
-        typename vector<Data>::iterator end() const {
-            return v.end();
-        }
+		typename vector<Data>::iterator begin() const {
+			return v.begin();
+		}
+
+		typename vector<Data>::iterator end() const {
+			return v.end();
+		}
 };
 
 #endif
